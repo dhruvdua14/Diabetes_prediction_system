6f3b56377b41ecd5623617e63078e1b008b601ea
Kalyani Jha <152086429+1kalyanijha@users.noreply.github.com>
2025-04-03 00:59:30 +0530

    Update shriya.py

--- shriya.py
+++ shriya.py
@@ -77,7 +77,7 @@ with st.sidebar:
     # creating a button for Prediction
     
     if st.button('Diabetes Test Result'):
-        diab_prediction = diabetes_model_two.predict([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+        diab_prediction = diabetes_model_two.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
         
         if (diab_prediction[0] == 1):
           diab_diagnosis = 'The person is diabetic'
